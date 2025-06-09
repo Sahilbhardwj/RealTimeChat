573aaeb559feb8eb2f9ca2910df98dbabfedc612
Sahil Bhardwaj <sahil.bhardwaj14123@gmail.com>
2025-06-09 20:12:41 +0530

    added userlabel

--- chatapp_FRONTEND/src/components/Topbar.tsx
+++ chatapp_FRONTEND/src/components/Topbar.tsx
@@ -2,8 +2,10 @@
 export function Topbar({myclicked,isitjoined,setCreateroom}){
   return(
   <div className='flex justify-end p-2 border'>
-        <div className="mr-96 bg-gray-700 text-black font-extrabold -600 rounded-md px-4">REALTIME CHAT</div>
-        <button className=" bg-gray-400 text-black px-4 py-2 mx-2 rounded font-bold" onClick={()=>{
+  
+        <div className="mr-96 bg-gray-700 text-white font-extrabold -600 rounded-md px-4">REALTIME CHAT</div>
+          <div className=" mr-28"></div>
+        <button className=" bg-gray-700 text-white px-4 py-2 mx-2 rounded font-bold" onClick={()=>{
            isitjoined(false)
            myclicked(true)
         }}>
--- chatapp_FRONTEND/src/components/joincard.tsx
+++ chatapp_FRONTEND/src/components/joincard.tsx
@@ -1,25 +1,34 @@
 import { useRef } from "react"
+import type { RefObject, Dispatch, SetStateAction } from "react";
 
-export function JoinCard({wsRef,setIsjoined,setMessage}){
-          
-        const mynewref=useRef(null)
-          function letsjoin(mymessage){
+interface MainProps {
+  wsRef: RefObject<WebSocket | null>; // or MutableRefObject if needed to mutate
+  setIsjoined: Dispatch<SetStateAction<boolean>>;
+  setMessage: Dispatch<SetStateAction<any[]>>;
+}
+
+export function JoinCard({wsRef,setIsjoined,setMessage}:MainProps){
+          const userref=useRef(null);
+          const mynewref=useRef(null)
+
+          function letsjoin(mymessage:string|null,username:string|null){
             const newdata= JSON.stringify({
             "type":"join",
             "payload":{
               "roomid": mymessage,
-              "name":"sahil"
+              "username":username
           }}
         )
-        wsRef.current.send(newdata)
+        wsRef.current?.send(newdata)
           }
     return(  
               <div className="grid grid-cols-3 gap-4 p-5 mt-40">
                   <div className="flex flex-col bg-gray-400 rounded-md h-60 w-96 col-start-2 py-16">
                         <input className="mx-4 my-1 py-3 px-3 font-bold rounded-md"ref={mynewref} type="text" placeholder='enter room id' />
+                        <input className="mx-4 my-1 py-3 px-3 font-bold rounded-md"ref={userref} type="text" placeholder='enter username' />
                         <button className='bg-white py-3 px-1 font-bold rounded-md mx-4 my-1' onClick={()=>{
-                          letsjoin(mynewref.current.value)
-                          setMessage((prev)=>[])
+                          letsjoin(mynewref.current?.value,userref.current?.value)
+                          setMessage(()=>[])
                           setIsjoined((c)=>!c)
                           }}>Join</button>
                   </div>
